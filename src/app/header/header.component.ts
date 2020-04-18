dc7a26b143d5e2c25b0b0f16803f708f2b15ed61
michaelkoder <michael.claude.web@gmail.com>
2020-04-19 00:25:52 +0200

    add url param to open website in english ok

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import {  ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +10,23 @@ import { TranslateService } from '@ngx-translate/core';
 export class HeaderComponent implements OnInit {
   
   constructor(    
-    public translate: TranslateService
+    public translate: TranslateService,
+    public route :ActivatedRoute
     ) { 
       translate.addLangs(['fr', 'en']);
       translate.setDefaultLang('fr');
     }
-  currentLang="";
+  currentLang:any="";
+  langParam:any='';
   ngOnInit(): void {
     console.log('currentLang > ');
     console.log(this.currentLang);
+    this.route.queryParams.subscribe(params => {
+      this.langParam = params['lang'];
+      if(this.langParam=='fr' || this.langParam=='en'){
+        this.translate.use(this.langParam);
+      }
+    });
   }
   switchLang(lang: string) {
     this.translate.use(lang);
